5d9c4fb90c60a2a2acf9e06b81cd3a65d9101698
jasdox <jason.d.cox07@gmail.com>
2022-12-20 22:15:21 -0500

    Completed Day 4

---
+++ Code/Day4/Code_Day4_Part2.py
@@ -0,0 +1,25 @@
+# Read and store input by line
+data = open("Documents\\GitHub\\AdventOfCode_2022\\Inputs\\day4.txt", 'r')
+lines = data.readlines()
+lines = [item.strip() for item in lines]
+
+# Initialize 
+counter = 0
+
+# Split the pairs and ranges into lists and change to integer
+for i in range(len(lines)):
+    lines[i] = lines[i].split(',')
+    lines[i][0] = lines[i][0].split('-')
+    lines[i][1] = lines[i][1].split('-')
+    for j in range(2):
+        lines[i][0][j] = int(lines[i][0][j])
+        lines[i][1][j] = int(lines[i][1][j])
+
+# Check if either end of the first range is inside the second range and vice versa and add to counter 
+for i in range(len(lines)):
+    a1, a2, b1, b2 = lines[i][0][0], lines[i][0][1], lines [i][1][0], lines[i][1][1]
+    if (a1 in range(b1, b2+1) or a2 in range(b1, b2+1)) or (b1 in range(a1, a2+1) or b2 in range(a1, a2+1)):
+        counter += 1
+
+print("Number fully contained:", counter)
+
